8edfa5234c2923f8dfd109fa1068bd70bc4a6cd1
ylzi91 <97354085+ylzi91@users.noreply.github.com>
2024-10-24 14:29:13 +0200

    Initial commit

---
+++ src/main/java/EsercizioG4Set5BE/yurilenzi/YurilenziApplication.java
@@ -0,0 +1,13 @@
+package EsercizioG4Set5BE.yurilenzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YurilenziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YurilenziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EsercizioG4Set5BE/yurilenzi/YurilenziApplicationTests.java
@@ -0,0 +1,13 @@
+package EsercizioG4Set5BE.yurilenzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YurilenziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
